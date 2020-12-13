24d72f2654447dcd111434a3a165246be217af16
atmanad <atmanad0001@gmail.com>
2020-12-13 12:18:59 +0530

    final

--- app.js
+++ app.js
@@ -89,4 +89,4 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-app.listen(process.env.PORT || 5000, () => console.log("server running"));
+app.listen(process.env.PORT || 5000);
